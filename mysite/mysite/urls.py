d993a7efe9357c071a468bac2678fa69ddd27d91
Piyush Tamoli <piyushmodi812@gmail.com>
2025-03-04 21:11:33 +0530

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import save_location
+from .views import save_location,home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
