3916b43952e80d3c6d4e184a3ecfdc11a98ec7f9
Piyush Tamoli <piyushmodi812@gmail.com>
2025-06-24 15:30:45 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -131,7 +131,7 @@ EMAIL_HOST = 'smtp.gmail.com'  # Use Gmail or any other email service provider
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'swapeatmail@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'anbr opdy syfm alud'  # Replace with your app password
+EMAIL_HOST_PASSWORD = 'gzhj erhx buhh lblu'  # Replace with your app password
 DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
 ALLOWED_HOSTS = ['track-location-xbev.onrender.com', 'localhost', '127.0.0.1']
 
